b8d811145f9b4647146c94da20867fdc6432b592
GustavoGomesDias <treeelevengg@gmail.com>
2022-07-12 13:04:43 -0300

    refactor: move a handle git commands to a git script

--- gitCommands.go
+++ gitCommands.go
@@ -105,3 +105,24 @@ func RunGitFunction(gitCmd *flag.FlagSet, commit *string, ammend *bool, push *st
 		fmt.Println("Use '--msg' for run commit command, '--am' for run rewrite last commit or '--msgp' for commit and push")
 	}
 }
+
+func HandleGitCommands() {
+	gitCmd := flag.NewFlagSet("g", flag.ExitOnError)
+	commit := gitCmd.String("msg", "", "Run commit command with message")
+	push := gitCmd.String("msgp", "", "Run commit command with message and push")
+	local := gitCmd.String("l", "", "Pass local to a push")
+	branch := gitCmd.String("br", "", "Pass branch to a push")
+	main := gitCmd.String("main", "", "Run commit and push in origin main")
+	ammend := gitCmd.Bool("am", false, "Run amend command")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Expected 'g' tag for git actions:")
+		fmt.Println("	commit message (--msg), amend command (--am), commit with push (--msgp, --l and --br) or commit with push in main (--main).")
+		os.Exit(1)
+	}
+
+	if os.Args[1] == "g" {
+		gitCmd.Parse(os.Args[2:])
+		RunGitFunction(gitCmd, commit, ammend, push, local, branch, main)
+	}
+}
--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
-
 func main() {
-	gitCmd := flag.NewFlagSet("g", flag.ExitOnError)
-	commit := gitCmd.String("msg", "", "Run commit command with message")
-	push := gitCmd.String("msgp", "", "Run commit command with message and push")
-	local := gitCmd.String("l", "", "Pass local to a push")
-	branch := gitCmd.String("br", "", "Pass branch to a push")
-	main := gitCmd.String("main", "", "Run commit and push in origin main")
-	ammend := gitCmd.Bool("am", false, "Run amend command")
-
-	if len(os.Args) < 2 {
-		fmt.Println("Expected commit message (--msg), amend command (--am), commit with push (--msgp, --l and --br) or commit with push in main (--main).")
-		os.Exit(1)
-	}
-
-	if os.Args[1] == "g" {
-		gitCmd.Parse(os.Args[2:])
-		RunGitFunction(gitCmd, commit, ammend, push, local, branch, main)
-	}
+	HandleGitCommands()
 }
